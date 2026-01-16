519552c077b4af80ee2b5726d129890ebea9161d
Scott Lewis <scottslewis@gmail.com>
2026-01-15 17:30:03 -0800

    Initial commit

---
+++ org.openmcptools.annotation/src/main/java/org/openmcptools/annotation/McpResource.java
@@ -0,0 +1,22 @@
+package org.openmcptools.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import java.lang.annotation.ElementType;
+
+@Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface McpResource {
+
+	String name() default "";
+	
+	String title() default "";
+	
+	String description() default "";
+	
+	String uri() default "";
+	
+	String mimeType() default "text/plain";
+}
